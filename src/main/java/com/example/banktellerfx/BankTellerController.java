0860dfdae606eb8008e8d21ae4dd9d460324e296
Tyler <tylersarno21@gmail.com>
2022-03-21 15:16:58 -0400

    bug fix caused by previous commit

--- src/main/java/com/example/banktellerfx/BankTellerController.java
+++ src/main/java/com/example/banktellerfx/BankTellerController.java
@@ -219,7 +219,7 @@ public class BankTellerController {
         ccButton.setSelected(false);
         sButton.setSelected(false);
         mmButton.setSelected(false);
-        myTextArea.setText("What would you like to do?");
+        //myTextArea.setText("What would you like to do?");
     }
 
     private void openAcc(Account acc) {
